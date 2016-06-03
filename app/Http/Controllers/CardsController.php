b0a41bfdc936b13693df44d03620b38e25941567
Germangalia <germangalia@iesebre.com>
2016-06-03 10:27:50 +0200

    Modification

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Card;
-use Illuminate\Support\Facades\Cache;
 
 class CardsController extends Controller
 {
     public function index()
     {
-        $cards = Cache::rememberForever(
-            'cards', function(){
-            return Card::with('notes')->get();
-        });
+        $cards = Card::with('notes')->get();
 
         return view('cards.index', compact('cards'));
     }
